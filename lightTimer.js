af1a40813b877b462bd32cdffe4b8979fc92a6ca
aihnato <bp111692>
2020-02-17 23:01:45 +0200

    some
    upgrade form, added timer

---
+++ lightTimer.js
@@ -0,0 +1,133 @@
+(function($) {
+
+	function appendStructure(obj){
+		$(obj).append(
+			'<div class="lightTimerBlock">'+
+			'<span class="lightTimerDaysSpan lightTimerСaptionSpan">Days</span>'+
+			'<span class="lightTimerDays lightTimerNumberSpan"></span>'+
+			'</div>'+
+			'<div class="lightTimerBlock">'+
+			'<span class="lightTimerHoursSpan lightTimerСaptionSpan">Hours</span>'+
+			'<span class="lightTimerHours lightTimerNumberSpan"></span>'+
+			'</div>'+
+			'<div class="lightTimerBlock">'+
+			'<span class="lightTimerMinutesSpan lightTimerСaptionSpan">Minutes</span>'+
+			'<span class="lightTimerMinutes lightTimerNumberSpan"></span>'+
+			'</div>'+
+			'<div class="lightTimerBlock">'+
+			'<span class="lightTimerSecondsSpan lightTimerСaptionSpan">Seconds</span>'+
+			'<span class="lightTimerSeconds lightTimerNumberSpan"></span>'+
+			'</div>'
+			);
+	}
+	function getTimeRemaining(deadline) {
+		var t = Date.parse(deadline) - Date.parse(new Date());
+		var seconds = Math.floor((t / 1000) % 60);
+		var minutes = Math.floor((t / 1000 / 60) % 60);
+		var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+		var days = Math.floor(t / (1000 * 60 * 60 * 24));
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+	function initializeClock(obj, objName, language, deadline) {
+		appendStructure(obj);
+
+		var clock = $(objName);
+		var days = $('.lightTimerDays');
+		var hours = $('.lightTimerHours');
+		var minutes = $('.lightTimerMinutes');
+		var seconds = $('.lightTimerSeconds');
+
+		function updateClock() {
+			var t = getTimeRemaining(deadline);
+			
+			days.html(('0' + t.days).slice(-2));
+			hours.html(('0' + t.hours).slice(-2));
+			minutes.html(('0' + t.minutes).slice(-2));
+			seconds.html(('0' + t.seconds).slice(-2));
+			
+			var captionSpan = [];
+
+			switch(language){
+				case('ru'):
+				captionSpan = [
+				['Дней','День','Дня'],
+				['Часов','Час','Часа'],
+				['Минут','Минута','Минуты'],
+				['Секунд','Секунда','Секунды'],
+				]
+				break;
+				case('ua'):
+				captionSpan = [
+				['Днів','День','Дні'],
+				['Годин','Година','Години'],
+				['Хвилин','Хвилина','Хвилини'],
+				['Секунд','Секунда','Секунди'],
+				]
+				break;
+			}
+
+			$('.lightTimerСaptionSpan').each(function(i, e) {
+				var strSpan = String($(e).siblings('span').html());
+				var lastStrSpan =  Number(strSpan[strSpan.length-1]); 
+
+				if(lastStrSpan == 0)
+				{
+					$(e).html(captionSpan[i][0]);
+				}
+				else if(lastStrSpan == 1)
+				{
+					$(e).html(captionSpan[i][1]);
+				}
+				else if(lastStrSpan > 1 && lastStrSpan <= 4)
+				{
+					$(e).html(captionSpan[i][2]);
+				}
+				else if(lastStrSpan >= 5 && lastStrSpan <= 9 )
+				{
+					$(e).html(captionSpan[i][0]);
+				}
+			});
+
+			if (t.total <= 0) {
+				clearInterval(timeinterval);
+				$('.lightTimerDays').html('0');
+				$('.lightTimerHours').html('0');
+				$('.lightTimerMinutes').html('0');
+				$('.lightTimerSeconds').html('0');
+			}
+		}
+
+		updateClock();
+
+		var timeinterval = setInterval(updateClock, 1000);
+	}
+
+	jQuery.fn.lightTimer = function(options) {
+
+		settings = $.extend({
+			language: '',
+			deadline: ''
+		}, options);
+
+		this.each(function() {
+			if(options){
+				$.extend(settings, options)
+			}
+		});
+
+		var objName = $(this)[0].className; 
+		var deadline = settings.deadline;
+		var language = settings.language;
+
+		return this.each(function() {
+			initializeClock(this, objName, language, deadline);
+		});
+
+	};
+}) (jQuery);
--- src/js/part/script.js
+++ src/js/part/script.js
@@ -1,32 +1,64 @@
+/* timer */
+
+$('.jumbo_timer_clock').lightTimer({
+  language: 'ru',
+  deadline: '31 Jul 2020 23:59:00 GMT+0300'
+});
+
+/* timer END */
+
 /* select transform */
 
 if($('body').find('.selectField').length != 0){
   $('.selectField').each(function(ind, elem){
     var select = $(elem).find('select');
     var option = $(elem).find('option');
-
+    
     $(select).hide();
     $(elem).append('<div class="select_dropdown">'+
-                    '<div class="dropdown_caption"></div>'+
-                    '<ul class="dropdown_list"></ul>'+
-                    '</div> ');
-
+    '<div class="dropdown_caption"></div>'+
+    '<ul class="dropdown_list"></ul>'+
+    '</div> ');
+    
+    $(elem).find('.dropdown_caption').html($(option).first().html());
     $.each(option, function(i, e) {
-
-      $(elem).find('.dropdown_list').append('<li class="dropdown_list_item">'+$(e).html()+'</li>');
+      
+      $(elem).find('.dropdown_list').append('<li class="dropdown_list_item" data-value="'+$(e).val()+'">'+$(e).html()+'</li>');
     });
   });
-
+  
 }
 
 /* select transform END */
 
 
 /* dropdown */
-  
+
 $('body').on('click', '.dropdown_caption', function() {
-  $(this).siblings('.dropdown_list').slideToggle('fast');
-  $(this).toggleClass('active');
+  // $(this).siblings('.dropdown_list').slideToggle('fast');
+  // $(this).toggleClass('active');
+
+  if($(this).is('.active')){
+    $(this).siblings('.dropdown_list').slideUp('fast');
+    $(this).removeClass('active');
+
+  }
+  else{
+    $('.dropdown_list').slideUp('fast');
+    $('.dropdown_caption').removeClass('active');
+
+    $(this).siblings('.dropdown_list').slideDown('fast');
+    $(this).addClass('active');
+    
+  }
+
+});
+$('body').on('click', '.dropdown_list_item', function() {
+  $(this).parents('.dropdown_list').slideUp('fast');
+  $(this).parents('.dropdown_list').siblings('.dropdown_caption').removeClass('active');
+  $(this).parents('.dropdown_list').siblings('.dropdown_caption').html($(this).html());
+  
+  $(this).parents('.selectField').find('select').val($(this).data('value'));
 });
 $(document).mouseup(function (e){
   var div = $(".dropdown_list, .dropdown_caption"); 
